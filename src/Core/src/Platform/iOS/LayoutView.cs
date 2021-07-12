a4e14f5d184cb621c560759e2c75b2303e3c2e74
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-07-12 19:17:17 +0200

    Add AppThemeBinding support in Android and iOS

--- src/Core/src/Platform/iOS/LayoutView.cs
+++ src/Core/src/Platform/iOS/LayoutView.cs
@@ -1,6 +1,7 @@
 using System;
 using CoreGraphics;
 using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Handlers;
 using ObjCRuntime;
 using UIKit;
 
@@ -43,9 +44,9 @@ namespace Microsoft.Maui
 			CrossPlatformMeasure?.Invoke(bounds.Width, bounds.Height);
 			CrossPlatformArrange?.Invoke(bounds);
 		}
-		
+
 		static bool? respondsToSafeArea;
-		
+
 		bool RespondsToSafeArea()
 		{
 			if (respondsToSafeArea.HasValue)
@@ -92,6 +93,17 @@ namespace Microsoft.Maui
 			{
 				CrossPlatformArrange = view.Arrange,
 			};
+		}
+
+		public override void TraitCollectionDidChange(UITraitCollection? previousTraitCollection)
+		{
+			if (CurrentView?.Handler is ElementHandler handler)
+			{
+				var application = handler.GetRequiredService<IApplication>();
+				application?.ThemeChanged();
+			}
+
+			base.TraitCollectionDidChange(previousTraitCollection);
 		}
 	}
 }
